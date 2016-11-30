001a16a4823d3786bc7e170455fb0cbf50dba05a
Thien Nguyen Le Quynh <nlqthien@gmail.com>
2016-11-30 13:09:00 +0700

    #82

--- dockerinitcontainer.sh
+++ dockerinitcontainer.sh
@@ -3,6 +3,12 @@ set -e
 
 cd /app/src/SimplCommerce.WebHost && dotnet ef database update
 
-psql --username postgres -d simplcommerce -a -f /app/src/Database/StaticData_Postgres.sql
+echo 'select count(*) from myvalue;' | psql --username postgres -d simplcommerce -t > varfile.txt
+if [ `cat varfile.txt` eq 0 ]; then
+  echo "first time --> create init data"
+  psql --username postgres -d simplcommerce -a -f /app/src/Database/StaticData_Postgres.sql
+else
+  echo "already have init data"
+fi
 
 cd /app/src/SimplCommerce.WebHost && dotnet run
