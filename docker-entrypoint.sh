164132d6951ffe43ed9804a7ae720b12e21cef63
Thien Nguyen <nlqthien@gmail.com>
2016-12-01 14:54:15 +0700

    dockerizing v2

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 cd /app/src/SimplCommerce.WebHost && dotnet ef database update
 
-echo 'select count(*) from "Core_User";' | psql --username postgres -d simplcommerce -t > /tmp/varfile.txt
+echo 'select count(*) from "Core_User";' | psql -h simpldb --username postgres -d simplcommerce -t > /tmp/varfile.txt
 if [ `cat /tmp/varfile.txt` -eq 0 ]; then
   echo "first time --> create init data"
-  psql --username postgres -d simplcommerce -a -f /app/src/Database/StaticData_Postgres.sql
+  psql -h simpldb --username postgres -d simplcommerce -a -f /app/src/Database/StaticData_Postgres.sql
 else
   echo "already have init data"
 fi
